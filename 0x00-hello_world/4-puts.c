069ad6561393275fcbe90a64b961593706782cb8
Tobechukwu Uzoho <uzohofranklin@yahoo.com>
2023-06-16 15:15:55 -0700

    0x00. C - Hello, World Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main() 
-{
-	char usePuts[] = "Programming is like building a multilingual puzzle";
+/**
+ *  function_name - using put function to  print
+ *  Return: returns 0 
+ */
+int main(void)
+{	
+	char usePuts[] = "\"Programming is like building a multilingual puzzle";
+	
 	puts(usePuts);
-	return 0;
+	
+	return (0);
 }
