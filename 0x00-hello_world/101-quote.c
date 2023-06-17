5a9af2bc2ee108b60fd6e133e3ceb3c723216f03
Tobechukwu Uzoho <uzohofranklin@yahoo.com>
2023-06-17 14:03:23 -0700

    0x00. C - Hello, World Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,9 @@
 
 int main(void)
 {
-	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char* msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";	
 	
-	write(2, message, sizeof(message) - 1);
-
+	write(2, msg, sizeof(msg) - 1);
+	
 	return (1);
 }
