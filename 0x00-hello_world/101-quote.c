35f9e36fbd351ba72c2a7e3c3f096a3e446445d4
Tobechukwu Uzoho <uzohofranklin@yahoo.com>
2023-06-17 14:20:34 -0700

    0x00. C - Hello, World Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,9 @@
 
 int main(void)
 {
-	const char* msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";	
-	
+	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, msg, strlen(msg));
-	
+
 	return (1);
 }
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 /**
  *  main - using the put function to  print
- *  Return: returns 0 
+ *  Return: returns 0
  */
 int main(void)
 {
 	char usePuts[] = "\"Programming is like building a multilingual puzzle";
-	
+
 	puts(usePuts);
-	
+
 	return (0);
 }
