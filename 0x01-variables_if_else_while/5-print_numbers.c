e1a301d2224e618d4da4dfa551cb85a43c52f515
Tobechukwu Uzoho <uzohofranklin@yahoo.com>
2023-06-18 15:09:38 +0000

    0x01. C - Variables Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int num = 1;
+
+	for (num = 1; num < 10; num++)
+	{
+		printf("%i\n", num);
+	}
+
+	return (0);
+}
