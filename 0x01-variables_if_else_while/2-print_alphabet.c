337882299d0f54bc3fc6f69df0d4572305d54442
Tobechukwu Uzoho <uzohofranklin@yahoo.com>
2023-06-18 12:51:43 +0000

    0x01. C - Variables Task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char alph = 'a';
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
+	return (0);
+}
