6ddf4894bf1bd9bb58a190e2ca84e140fd9bc91e
Tobechukwu Uzoho <uzohofranklin@yahoo.com>
2023-06-18 12:40:52 +0000

    0x01. C - Variables Task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 		printf("Last digit of %i is %i and is 0\n", n, m);
 	} else if ((m < 6) && (m != 0))
 	{
-		printf("last digit of %i is %i and is less than 6 and not 0\n", n, m);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, m);
 	}
 	return (0);
 }
