f39524aba777765dd56295bddd478065988f8b0a
Tobechukwu Uzoho <uzohofranklin@yahoo.com>
2023-06-19 17:49:42 +0000

    0x01. C - Variables Task12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int num1;
 
-	for (num1 = 0; num1 <= 99; num1++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
 		int num2;
 
@@ -21,7 +21,7 @@ int main(void)
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 
-			if (num1 < 99 || num2 < 99)
+			if (num1 < 98 || num2 < 99)
 			{
 				putchar(',');
 				putchar(' ');
