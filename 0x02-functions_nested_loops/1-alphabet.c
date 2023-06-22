770e38eb1cf41db7554bafbd2e80625d90f15dbf
Tobechukwu Uzoho <uzohofranklin@yahoo.com>
2023-06-22 19:19:04 +0000

    C - Functions Task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * alphabet - prints lowercase alphabet
+ */
+
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+
+	_putchar('\n');
+}
