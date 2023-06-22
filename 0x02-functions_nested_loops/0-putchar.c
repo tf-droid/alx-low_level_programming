6dfcde98e3ab1f55e84f87fc1a2d387a5c72e021
Tobechukwu Uzoho <uzohofranklin@yahoo.com>
2023-06-22 18:36:08 +0000

    corrected the declaration of char in Task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,9 +8,31 @@
 
 int main(void)
 {
-	char c[] = "_putchar";
+	char c = '_';
 
 	_putchar(c);
+
+	c = 'p';
+	_putchar(c);
+
+	c = 'u';
+	_putchar(c);
+
+	c = 't';
+	_putchar(c);
+
+	c = 'c';
+	_putchar(c);
+
+	c = 'h';
+	_putchar(c);
+
+	c = 'a';
+	_putchar(c);
+
+	c = 'r';
+	_putchar(c);
+
 	_putchar('\n');
 
 	return (0);
