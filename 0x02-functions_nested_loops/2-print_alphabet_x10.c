ad3a2914f9ad1923a4838f97cfa1e74558434398
Tobechukwu Uzoho <uzohofranklin@yahoo.com>
2023-06-22 19:52:51 +0000

    C - Functions Task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ */
+
+void print_alphabet_x10(void)
+{
+	int numOfPrint;
+	char alph;
+
+	for (numOfPrint = 0; numOfPrint <= 10; numOfPrint++)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+			_putchar(alph);
+		}
+		_putchar('\n');
+	}
+}
