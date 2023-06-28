fedcae198d361158896d487ca6d00c650d1eae21
Tobechukwu Uzoho <uzohofranklin@yahoo.com>
2023-06-28 19:16:41 +0000

    C-Pointers Task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer and updates its value
+ * @*n: int pointer as parameter
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
